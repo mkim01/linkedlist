6adfc96abe0f7a20d94669d208fe0b296fd22c2d
Minuk Kim <mkim01@stuy.edu>
2019-10-27 16:11:29 -0400

    wrote free_list and remove_node

--- linkedlist.c
+++ linkedlist.c
@@ -3,10 +3,12 @@
 #include "header.h"
 
 void print_list(struct node *mynode){
+  printf("[");
   while (mynode != NULL){
     printf("%d", mynode->i);
     mynode = mynode -> next;
   }
+  printf("]");
 }
 
 struct node * insert_front(struct node *mynode, int value){
@@ -17,9 +19,28 @@ struct node * insert_front(struct node *mynode, int value){
 }
 
 struct node * free_list(struct node *mynode){
+  while(mynode != NULL){
+    struct node* holder = mynode;
+    free(holder);
+    holder = NULL;
+    mynode = mynode->next;
+  }
   return mynode;
 }
 
 struct node * remove_node(struct node *front, int data){
+  struct node *prev = NULL;
+  struct node *curr = front;
+  while(front != NULL){
+    if(front->i == data){
+      struct node *holder = curr;
+      if(prev == NULL){
+        front = front->next;} //when front is the first node, front should point to the next node to remove the first one
+      else{
+        prev->next = holder->next;} // the pointer of prev should skip to the following node of deleted one
+      free(front);
+      front = NULL;
+      }
+    }
   return front;
 }
