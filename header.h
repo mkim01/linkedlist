20fc0879d1bfc0ed43c56f9dc85dad0245d0de7f
Minuk Kim <mkim01@stuy.edu>
2019-10-27 14:10:49 -0400

    update header file

--- header.h
+++ header.h
@@ -1,3 +1,5 @@
-struct node * insert_front(struct node *mynode, int value);
-struct node * free_list(struct node *mynode);
-void print_list(struct node *mynode);
+struct node { int i; struct node *next;};
+void print_list(struct node * mynode);
+struct node * insert_front(struct node * mynode, int value);
+struct node * free_list(struct node * mynode);
+struct node * remove_node(struct node *front, int data);
